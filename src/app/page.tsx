f651d3d4d61cb2d362736f9f4bf867fc53cd3b55
Riaz-Hussain-Saifi <infosaifideveloper@gmail.com>
2024-12-17 07:05:04 +0500

    something deleted

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,15 @@
 import React from 'react'
 import HomePage from '@/components/HomePages/HomeHero'
 import FoodCategory from '@/components/HomePages/FoodCatagery' 
-import WhyChouseUs from '@/components/HomePages/WhyCouseUs'
 import OurChefs from '@/components/HomePages/OurChefs'
 // import ChefsHero from '@/components/HomePages/ChefsHero'
-// import ChefGrid from '@/components/HomePages/ChefGrid'
+
 
 const Home = () => {
   return (
     <div>
       <HomePage />
       <FoodCategory />
-      <WhyChouseUs />
       <OurChefs />
       {/* <ChefGrid /> */}
     </div>
